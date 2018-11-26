41f028cb786418158256020354951d3df3ad35c9
jk125262 <jkeller@eonerc.rwth-aachen.de>
2018-11-26 18:23:28 +0100

    deltatests: shell scripts for compilation and execution

---
+++ deltatests/compilequick.sh
@@ -0,0 +1,76 @@
+#!/bin/zsh
+
+# Compile shemat_suite executable from model directory
+
+#------------------------------------------------------------------
+#-----------------------Variables ---------------------------------
+#------------------------------------------------------------------
+model_dir="${HOME}/SHEMAT-Suite_Models/deltatests"
+make_dir="${HOME}/SHEMAT-Suite"
+
+shem_type="fw"			# "sm", "fw"
+shem_type_name="fw"	# "sm_sgsim", "fw"
+
+props="const"
+
+compiler="ling64"	       # "ling64","lini64"
+compiler_name="64gnu"	       # "64gnu","64int"
+
+flags="omp noplt vtk nohdf -j16" # Flags: "omp","debug","noplt","novtk","nohdf"
+
+#Make-directory existence check
+if [ ! -d ${make_dir} ]
+then
+    echo "   Makefile directory"
+    echo ${make_dir}
+    echo "   does not exist."
+    exit 1
+fi
+
+# Go to make_dir
+pushd ${make_dir}
+
+# Get git branch name
+git_branch=$(git rev-parse --abbrev-ref HEAD)
+
+#New executable name
+new_exe_name="shem_${shem_type_name}${compiler_name}_${props}_${git_branch}.x"
+
+#Clean make-directory
+gmake clean
+
+#Compilation command 
+gmake ${shem_type} COMPTYPE=${compiler} PROPS=${props} ${flags}
+
+# Catch compilation errors
+if [ $? -ge 1 ];
+then
+    echo ""
+    echo "------------------------------------"
+    echo "compilequick.sh: Compilation aborted"
+    exit 1
+fi
+
+#Rename executable
+rename shem_${shem_type_name}${compiler_name}_${props}.x ${new_exe_name} shem_${shem_type_name}${compiler_name}_${props}.x
+
+#Move executable
+mv ${new_exe_name} ${model_dir}
+
+# Clean make-directory
+gmake clean
+
+# Create and move tgz Backup
+gmake tgz
+mv *.tgz ${model_dir}
+
+# Echo paths
+echo "--------------------------------------------------------"
+echo "Compilation path:"
+pwd
+popd
+
+echo
+echo "Current path:"
+pwd
+echo
---
+++ deltatests/exequick.sh
@@ -0,0 +1,63 @@
+#!/bin/zsh
+
+# Run SHEMAT-Suite executable in model directory
+
+#------------------------------------------------------------------
+#-----------------------Variables ---------------------------------
+#------------------------------------------------------------------
+shemat_suite_models_dir="${HOME}/SHEMAT-Suite_Models"
+
+shem_type_name="fw"			# "sm", "fw"
+compiler_name="64gnu"	       # "64gnu","64int"
+props="const"
+model_name="TheisProblem"
+
+git_branch="release"
+
+# deltatests_dir
+deltatests_dir="${shemat_suite_models_dir}/deltatests"
+
+# model_dir
+model_dir="${shemat_suite_models_dir}/${shem_type_name}_${props}_${model_name}"
+
+# exe_name
+exe_name="shem_${shem_type_name}${compiler_name}_${props}_${git_branch}.x"
+
+# deltatests_dir existence check
+if [ ! -d ${deltatests_dir} ]
+then
+    echo "   deltatests_dir"
+    echo ${deltatests_dir}
+    echo "   does not exist."
+    exit 1
+fi
+
+# model_dir existence check
+if [ ! -d ${model_dir} ]
+then
+    echo "   model_dir"
+    echo ${model_dir}
+    echo "   does not exist."
+    exit 1
+fi
+
+# Go to model_dir
+pushd ${deltatests_dir}
+
+# exe_name existence check
+if [ ! -e ${exe_name} ]
+then
+    echo "   exe_name"
+    echo ${exe_name}
+    echo "   does not exist."
+    exit 1
+fi
+
+# Copy executable
+cp ${exe_name} ${model_dir}
+popd
+
+# Run executable
+pushd ${model_dir}
+./${exe_name}
+popd
